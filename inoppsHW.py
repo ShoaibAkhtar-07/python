ffe6fbe38bc80c925db55ad588b95a7006c0775f
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-05-14 21:46:30 +0530

    Update inoppsHW.py

--- inoppsHW.py
+++ inoppsHW.py
@@ -80,7 +80,6 @@ class vector:
         
     def __add__(self,other):
         return vector(self.x + other.x, self.y + other.y, self.z + other.z)
-
     
     def __mul__(self,other):
         result = self.x * other.x + self.y * other.y + self.z *other.z
@@ -88,9 +87,7 @@ class vector:
     
     def __str__(self):
         return f"{self.x}i + {self.y}j + {self.z}k"
-        
 
-        
 v1 = vector(1,2,3)
 v2 = vector(4,5,6)
 v3 = vector(7,8,9)
