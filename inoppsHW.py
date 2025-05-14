5e7d36e52382cc16c47eff3d6c712f32a41d86e5
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-05-14 21:41:44 +0530

    Update inoppsHW.py

--- inoppsHW.py
+++ inoppsHW.py
@@ -54,18 +54,49 @@
 # print(f"TOTAL salary after increment = {e.totalsalaryafterincrement}")
 # print(f"Incremented amount = {e.salaryafterincrement}")
 
-# PROBLEM 4
+# # PROBLEM 4
 
-class complex:
-    def __init__(self,a,b):
-        self.a=a
-        self.b=b
-    def __add__(self,s2):
-        return complex(self.a+s2.a,self.b+s2.b)
-    def __str__(self):
-        return f"{self.a}+{self.b}"
+# class complex:
+#     def __init__(self,a,b):
+#         self.a=a
+#         self.b=b
+#     def __add__(self,s2):
+#         return complex(self.a+s2.a,self.b+s2.b)
+#     def __str__(self):
+#         return f"{self.a}+{self.b}"
 
         
-s1 = complex(1,2)
-s2 = complex(3,4)
-print(s1+s2)
+# s1 = complex(1,2)
+# s2 = complex(3,4)
+# print(s1+s2)
+
+# PROBLEM 5
+
+class vector:
+    def __init__(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
+        
+    def __add__(self,other):
+        return vector(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    
+    def __mul__(self,other):
+        result = self.x * other.x + self.y * other.y + self.z *other.z
+        return result
+    
+    def __str__(self):
+        return f"{self.x}i + {self.y}j + {self.z}k"
+        
+
+        
+v1 = vector(1,2,3)
+v2 = vector(4,5,6)
+v3 = vector(7,8,9)
+
+print(v1+v2)
+print(v1*v2)
+
+print(v1+v3)
+print(v1*v3)
