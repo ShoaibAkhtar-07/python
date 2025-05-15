9d45c5710be22fba6d423d9d6ba47ae133941028
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-05-16 00:20:42 +0530

    Create exceptionhandling.py

---
+++ exceptionhandling.py
@@ -0,0 +1,23 @@
+# 1
+
+try :
+    a = int(input("enter the number : "))
+    print(a)
+except Exception as e:
+    print(e)
+    print("Thank You")
+
+else:    
+   print("I am in side else")
+
+
+
+# # 2
+
+# a = int(input("Enter the value : "))
+# b = int(input("Enter the value : "))
+# if(b==0):
+#     raise ZeroDivisionError("PLEASE ~~ enter any number except '0' ~~")
+# else:
+#     print(f"--{a/b}--")
+
