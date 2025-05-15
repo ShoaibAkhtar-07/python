a07bf9bbb9a4ebdac52b184d9a40cf7de401baa0
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-05-15 17:05:41 +0530

    Create tydef.py

---
+++ tydef.py
@@ -0,0 +1,22 @@
+# # 1
+
+# n = 5
+# n : int = 5
+ 
+# def sum(a : int,b : int) -> int:
+#     return a+b
+# print(sum(4,5))
+    
+    
+    
+# 2
+
+from typing import List,Tuple,Dict,Union
+
+number :list[int] = [1,2,3,4,5]
+persons : tuple[str,int] = ("shoaib",7)
+marks : dict[str,int]={"akhtar" : 100,
+                       "sameer" : 50,
+                       "vijay" : 66
+                       }
+id : Union[str,int]="sa07"
