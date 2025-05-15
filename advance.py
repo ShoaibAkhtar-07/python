1f6ffea2f25f0910559c5388657e8f0b3cf75657
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-05-15 17:05:33 +0530

    Create advance.py

---
+++ advance.py
@@ -0,0 +1,41 @@
+# ## WALRUS OPERATOR
+
+# data = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# results = [n for n in data if n > 5]
+# print(results)
+
+# data = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# print([n for n in data if (x := n) > 5])
+
+
+
+# # 2
+
+# if(value := len("shoaib akhtar"))>0:
+#     print(value)
+    
+# if (value := len("shoaib akhtar") - "shoaib akhtar".count(" ")) > 0:
+#     print(value)
+
+
+
+# # 3
+
+# while(n:=input("enter : "))!="quit":
+#     print(n)                           
+# # n = input("enter :")
+# # while(n != "quit"):
+# #   print(f"you entered {n}")
+# #   n = input("enter :")
+  
+   
+   
+# 4
+
+results = [y for x in range(10) if (y := x**2) > 10]
+print(results)
+
+
+                                     
+
+
