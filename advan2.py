492a500b63151460bd3bb683c44a65a2977c6cd6
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-05-17 13:23:39 +0530

    Create advan2.py

---
+++ advan2.py
@@ -0,0 +1,17 @@
+# # # LAMDA
+
+
+# # def square(n):
+# #     print(n*n)
+# square = lambda x : x*x
+# n = int(input("Enter the number : "))
+# print(square(n))
+
+
+
+# JOIN
+
+s = ["shoaib","akhtar","nazir"]
+final= "::".join(s)
+print(final)
+print(s)
