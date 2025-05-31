e0b2f7feebd59b4f0678d03efa18c5e13d3c8987
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-05-31 17:16:57 +0530

    Create musiclibrary.py

---
+++ python mega projects 29/musiclibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "daylight" : "https://youtu.be/MoN9ql6Yymw",
+    "wavinflag": "https://youtu.be/6q0d2b1a9e4",
+    "it's time" : "https://youtu.be/sENM2wA_FTg",        
+    "overcomer" : "https://youtu.be/z29olPjFbqg"
+
+}
