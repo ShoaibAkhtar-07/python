ed370f30245cca34581c55fddab6c3933ac56d64
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-05-31 21:57:36 +0530

    Update musiclibrary.py

--- python mega projects 29/musiclibrary.py
+++ python mega projects 29/musiclibrary.py
@@ -1,7 +1,14 @@
 music = {
+    "can you hear the music" : "https://www.youtube.com/watch?v=LYigiwbaX_U&pp=ygUWY2FuIHlvdSBoZWFyIHRoZSBtdXNpYw%3D%3D",
     "daylight" : "https://youtu.be/MoN9ql6Yymw",
-    "wavinflag": "https://youtu.be/6q0d2b1a9e4",
+    "waving flag": "https://youtu.be/6q0d2b1a9e4",
     "it's time" : "https://youtu.be/sENM2wA_FTg",        
-    "overcomer" : "https://youtu.be/z29olPjFbqg"
+    "overcomer" : "https://youtu.be/z29olPjFbqg",
+    "sparrows" : "https://www.youtube.com/watch?v=wRJZQFRyZ6s&pp=ygUMc3BhcnJvdyBzb25n",
+    "cliche"   : "https://www.youtube.com/watch?v=m4DVbm9zeR0&pp=ygUKY2xpY2hlIG1naw%3D%3D",
+    "dead men" : "https://www.youtube.com/watch?v=iD5P9bDWX8M&pp=ygUWZGVhZCBtYW4gZGF2aWQga3VzaG5lcg%3D%3D",
+    "treat you better" : "https://www.youtube.com/watch?v=lY2yjAdbvdQ&pp=ygUQdHJlYXQgeW91IGJldHRlcg%3D%3D",
+    "stitches" : "https://www.youtube.com/watch?v=VbfpW0pbvaU&pp=ygUIc3RpdGNoZXM%3D",
+    "hall of fame" :"https://youtu.be/3Kxf2dHlDpQ",
 
 }
