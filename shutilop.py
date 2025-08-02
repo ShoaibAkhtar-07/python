6ddc5b583aefa4aec14b2f99811641e0c386bc4d
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-08-03 01:51:25 +0530

    Create shutilop.py

---
+++ shutilop.py
@@ -0,0 +1,8 @@
+import shutil
+import os
+
+# shutil.copy("main.py", "main2.py")
+# shutil.copytree("python mega projects 29", "python mega projects 29 (file 2)")
+# shutil.move(".python mega projects 29/file.file", "file.file")
+# shutil.rmtree("mytutorial")
+os.remove("file.file")
